56de9a4d50d5818a0b1a4066b08710ab9208ec98
Tymur <risedack1@gmail.com>
2021-12-03 12:47:08 +0300

    fix header popup adain

--- js/main.js
+++ js/main.js
@@ -168,6 +168,25 @@ $(function () {
             scrollTop: top
         }, 1500);
     });
+
+
+    //E-mail Ajax Send
+    $(".cooperation__form").submit(function () { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function () {
+            alert("Thank you!");
+            setTimeout(function () {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
 });
 
 Calamansi.autoload();
